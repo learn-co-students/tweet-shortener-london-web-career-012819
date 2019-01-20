7518f93e43207c922997997a6757b969d7f3606c
Kian Edalat <akedalat@gmail.com>
2019-01-20 18:54:54 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+require "pry"
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2", "two" => "2", "too" => "2",
+    "for" => "4", "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  to_array = string.split(" ")
+  to_array.collect do |element|
+    if dictionary.keys.include?(element.downcase)
+          element = dictionary[element.downcase]
+        else
+          element
+      end
+    end.join(" ")
+end

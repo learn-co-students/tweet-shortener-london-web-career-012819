214010f6ce4295eb2693a3d605c0376ac5540105
Kian Edalat <akedalat@gmail.com>
2019-01-20 21:00:18 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -22,3 +22,26 @@ def word_substituter(string)
       end
     end.join(" ")
 end
+
+def bulk_tweet_shortener(string)
+  string.each do |tweet|
+puts word_substituter(tweet)
+end
+end
+
+def selective_tweet_shortener(string)
+    if string.length > 140
+word_substituter(string)
+else
+  string
+end
+end
+
+def shortened_tweet_truncator(string)
+word_substituter(string)
+  if string.length > 140
+    string = string[0..136]
+    string += "..."
+  end
+return string
+end

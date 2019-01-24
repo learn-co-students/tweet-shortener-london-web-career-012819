140e25c6a5f240cbd5268797ba889c6b2531077b
SofiaFredriksson <sofiapkf@gmail.com>
2019-01-24 11:34:20 +0000

    Done

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,8 +2,11 @@ require 'pry'
 
 def dictionary
   dictionary_hash = { "hello" => "hi",
-                     ["to", "two", "too"] => "2",
-                     ["for", "four"] => "4",
+                     "to" => "2",
+                     "two" => "2",
+                     "too" => "2",
+                     "for" => "4",
+                     "four" => "4",
                      "be" => "b",
                      "you" => "u",
                      "at" => "@",
@@ -14,18 +17,12 @@ end
 
 def word_substituter(tweet)
   tweet_arr = tweet.split(/ /)
-  dictionary_keys = dictionary.keys.flatten
-
-    new_arr = tweet_arr.each do |tweet|
-      dictionary_keys.each do |word|
-      if tweet == word
-        dictionary.each do |k, v|
-        if word == k
-        word = v
+    tweet_arr.each_with_index do |tweet, index|
+      dictionary.each do |key, value|
+      if key == tweet
+        tweet_arr[index] = value
+      end
     end
-    end
-    end
-    end
-    end
-    new_arr.join(" ")
+  end
+    tweet_arr.join(" ")
  end

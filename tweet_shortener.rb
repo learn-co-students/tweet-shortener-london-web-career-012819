2aa0f0d04f79e8a76487e49498b0fa5d49be1cfc
SofiaFredriksson <sofiapkf@gmail.com>
2019-01-24 10:43:43 +0000

    commit 1

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  dictionary_hash = { "hello" => "hi",
+                     ["to", "two", "too"] => "2",
+                     ["for", "four"] => "4",
+                     "be" => "b",
+                     "you" => "u",
+                     "at" => "@",
+                      "and" => "&"
+    }
+
+end
+
+def word_substituter(tweet)
+  tweet_arr = tweet.split(/ /)
+  dictionary_keys = dictionary.keys.flatten
+
+    new_arr = tweet_arr.each do |tweet|
+      dictionary_keys.each do |word|
+      if tweet == word
+        dictionary.each do |k, v|
+        if word == k
+        word = v
+    end
+    end
+    end
+    end
+    end
+    new_arr.join(" ")
+ end
